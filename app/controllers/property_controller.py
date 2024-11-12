49466b552feb866dff8ffd1b5ac240212ca40055
Lakshan Wijesekara <int.lakshan@creativesoftware.com>
2024-11-12 16:49:48 +0530

    * Handle data requests, data process at controller class * Handle HTTP responses at controller class * Remove the data requests and HTTP respones from services class * At Get method, simplify the code by using one find statement and change logic

--- app/controllers/property_controller.py
+++ app/controllers/property_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint #jsonify converts python objects into JSON format
+from flask import Blueprint, jsonify, request #jsonify converts python objects into JSON format
 from services.property_services import PropertyServices
 from services.http_responses import HttpResponse
 from flask_cors import cross_origin
@@ -20,27 +20,83 @@ class PropertyController:
     @cross_origin(supports_credentials=True)  
     @api_blueprint.route('/properties', methods=['GET']) #API endpoint
     def get_properties():
-        return property_services.get_all()
-    
+        try:
+            selectedCity = request.args.get('selectedCity')
+            propertyName = request.args.get('propertyName')
+            return property_services.get_all(selectedCity, propertyName)
+        except Exception as error:
+            error_at_exception = http_responses.errorResponse(str(error))
+            return jsonify(error_at_exception)
+        
     @cross_origin(supports_credentials=True)
     @api_blueprint.route('/properties/<id>', methods=['GET'])    
     def get_property(id):
-        return property_services.get_property(id)
+        try:
+            retrieved_property = property_services.get_property(id)
+            if retrieved_property:
+                success_response = http_responses.successResponse(retrieved_property)
+                return jsonify(success_response)
+            else:
+                error_message = "Property id does not match with any records!"
+                error_response = http_responses.errorResponse(error_message)
+                return jsonify(error_response)
+
+        except Exception as error:
+            error_at_exception = http_responses.errorResponse(str(error))
+            return jsonify(error_at_exception)  
 
     @cross_origin(supports_credentials=True)
     @api_blueprint.route('/properties', methods=['POST'])
     def insert_property():
-        return property_services.insert_property()
-        
+        try:
+            property_payload = request.get_json()
+            inserted_property = property_services.insert_property(property_payload)
+            if inserted_property:
+                success_response = http_responses.successResponse(inserted_property)
+                return jsonify(success_response)
+            else:
+                error_message = "No document is available to insert!"
+                error_response = http_responses.errorResponse(error_message)
+                return jsonify(error_response)
+        except Exception as error:
+            error_at_exception = http_responses.errorResponse(str(error))
+            return jsonify(error_at_exception) 
+
     @cross_origin(supports_credentials=True)
     @api_blueprint.route('/properties/<id>', methods=['PUT'])
     def update_property(id):
-        return property_services.update_property(id)  
+        try:
+            property_payload = request.get_json()
+            updated_property = property_services.update_property(id, property_payload)
+            if updated_property:
+                success_message = "The identified property updated successfully"
+                success_response = http_responses.successResponse(success_message)
+                return jsonify(success_response)
+            else:
+                error_message = "No record found to update"
+                error_response = http_responses.errorResponse(error_message)
+            return jsonify(error_response)  
+        except Exception as error:
+            error_at_exception = http_responses.errorResponse(str(error))
+            return jsonify(error_at_exception)
 
     @cross_origin(supports_credentials=True)
     @api_blueprint.route('/properties/<id>', methods=['DELETE'])    
     def deactivate_property(id):
-        return property_services.deactivate_property(id)
+        try:
+            deleted_property = property_services.deactivate_property(id)
+            if deleted_property:
+                success_message = "The identified property was deactivated successfully"
+                success_response = http_responses.successResponse(success_message)
+                return jsonify(success_response)
+            else:
+                error_message = "No record found to deactivate"
+                error_response = http_responses.errorResponse(error_message)
+                return jsonify(error_response)
+        
+        except Exception as error:
+            error_at_exception = http_responses.errorResponse(str(error))
+            return jsonify(error_at_exception) 
         
     #This code provides a basic implementation of a Flask API endpoint for retrieving all properties from a MongoDB database. 
     #The jsonify function is used to create a JSON response that can be sent back to the client.
--- app/services/property_services.py
+++ app/services/property_services.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request
+from flask import jsonify
 from services.http_responses import HttpResponse
 from services.util_services import UtilService
 from services.db_connection import MongoDBConnection, property_collection_name
@@ -11,104 +11,50 @@ class PropertyServices:
         self.http_responses = HttpResponse()
         self.property_collection = database_connection.get_propertyCollection(property_collection_name)
 
-    def get_all(self ):
-        try:
-            get_property_collection =  self.property_collection
-            properties = get_property_collection.find() #This returns a cursor object if not for list keyword(better for large datasets)
-            json_properties = self.convert_properties.convert_cursor_object(properties)
-            retrieve_all_response = self.http_responses.successResponse(json_properties)
-            # Query parameters for filtering
-            selectedCity = request.args.get('selectedCity')
-            propertyName = request.args.get('propertyName')
-            if selectedCity or propertyName:
-                # Argument gets the selectedCity and propertyName from the URL and checks if the returned value can be found in the document list
-                filter_query = {}
-                if selectedCity:
-                    filter_query['selectedCity'] = {"$regex": f"^{selectedCity}", "$options": "i"} #Here, f is the F string where the string to be more concise, ^ is the start of string, options make the filter not case sensitive
-                if propertyName:
-                    filter_query['propertyName'] = {"$regex": f"^{propertyName}", "$options": "i"}
-                filter_query['is_active'] = True
-                results = get_property_collection.find(filter_query)
-                json_results = self.convert_properties.convert_cursor_object(results)
-                filter_response = self.http_responses.successResponse(json_results)
-                counter = get_property_collection.count_documents(filter_query)
+    def get_all(self,selectedCity, propertyName ):
+        get_property_collection =  self.property_collection
+        # Query parameters for filtering
+        filter_query = {}
+        if selectedCity or propertyName:
+            # Argument gets the selectedCity and propertyName from the URL and checks if the returned value can be found in the document list
+            if selectedCity:
+                filter_query['selectedCity'] = {"$regex": f"^{selectedCity}", "$options": "i"} #Here, f is the F string where the string to be more concise, ^ is the start of string, options make the filter not case sensitive
+            if propertyName:
+                filter_query['propertyName'] = {"$regex": f"^{propertyName}", "$options": "i"}
+            filter_query['is_active'] = True
 
-                if counter>0:
-                    return jsonify(filter_response)
-                else:
-                    return jsonify([])
-                
-            else:
-                 return jsonify(retrieve_all_response)
-        except Exception as error:
-            error_at_exception = self.http_responses.errorResponse(str(error))
-            return jsonify(error_at_exception)
+        properties = get_property_collection.find(filter_query)
+        #This returns a cursor object if not for list keyword(better for large datasets)
+        json_properties = self.convert_properties.convert_cursor_object(properties)
+        retrieve_all_response = self.http_responses.successResponse(json_properties)
+        counter = len(json_properties)
+        if counter>0:
+            return jsonify(retrieve_all_response)
+        else:
+            return jsonify([])
     
     def get_property(self,id):    
-        try:
-            get_property_collection =  self.property_collection
-            property_id = self.convert_properties.convert_object_id(id)
-            retrieved_property = get_property_collection.find({'_id': property_id}) 
-            if retrieved_property:
-                retrieved_property_object = self.convert_properties.convert_cursor_object(retrieved_property)
-                success_response = self.http_responses.successResponse(retrieved_property_object)
-                return jsonify(success_response)
-            else:
-                error_message = "Property id does not match with any records!"
-                error_response = self.http_responses.errorResponse(error_message)
-                return jsonify(error_response)
-        except Exception as error:
-            error_at_exception = self.http_responses.errorResponse(str(error))
-            return jsonify(error_at_exception)  
-
-    def insert_property(self):
-        try:
-            get_property_collection =  self.property_collection
-            property_payload = request.get_json()
-            inserted_property = get_property_collection.insert_one(property_payload)
-            if inserted_property:
-                inserted_property_id = str(inserted_property.inserted_id)
-                success_response = self.http_responses.successResponse(inserted_property_id)
-                return jsonify(success_response)
-            else:
-                error_message = "No document is available to insert!"
-                error_response = self.http_responses.errorResponse(error_message)
-                return jsonify(error_response)
-        except Exception as error:
-            error_at_exception = self.http_responses.errorResponse(str(error))
-            return jsonify(error_at_exception) 
+        get_property_collection =  self.property_collection
+        property_id = self.convert_properties.convert_object_id(id)
+        retrieved_property = get_property_collection.find({'_id': property_id}) 
+        retrieved_property_object = self.convert_properties.convert_cursor_object(retrieved_property)
+        return retrieved_property_object
+    
+    def insert_property(self, property_payload):
+        get_property_collection =  self.property_collection
+        inserted_property = get_property_collection.insert_one(property_payload)
+        inserted_property_id = str(inserted_property.inserted_id)
+        return inserted_property_id
+    
+    def update_property(self,id, property_payload):
+        get_property_collection =  self.property_collection
+        property_id = self.convert_properties.convert_object_id(id)
+        updated_property = get_property_collection.update_one({"_id": property_id}, {"$set":property_payload})
+        return updated_property
         
-    def update_property(self,id):
-        try:
-            get_property_collection =  self.property_collection
-            property_id = self.convert_properties.convert_object_id(id)
-            property_payload = request.get_json()
-            updated_property = get_property_collection.update_one({"_id": property_id}, {"$set":property_payload})
-            if updated_property:
-                success_message = "The identified property updated successfully"
-                success_response = self.http_responses.successResponse(success_message)
-                return jsonify(success_response)
-            else:
-                error_message = "No record found to update"
-                error_response = self.http_responses.errorResponse(error_message)
-                return jsonify(error_response)
-        except Exception as error:
-            error_at_exception = self.http_responses.errorResponse(str(error))
-            return jsonify(error_at_exception)
-
     def deactivate_property(self, id):
-        try:
-            get_property_collection=  self.property_collection
-            property_id = self.convert_properties.convert_object_id(id)
-            deleted_property = get_property_collection.update_one({"_id": property_id},{"$set": {"is_active": False}})
-            if deleted_property:
-                success_message = "The identified property was deactivated successfully"
-                success_response = self.http_responses.successResponse(success_message)
-                return jsonify(success_response)
-            else:
-                error_message = "No record found to deactivate"
-                error_response = self.http_responses.errorResponse(error_message)
-                return jsonify(error_response)
-        except Exception as error:
-            error_at_exception = self.http_responses.errorResponse(str(error))
-            return jsonify(error_at_exception)        
+        get_property_collection=  self.property_collection
+        property_id = self.convert_properties.convert_object_id(id)
+        deleted_property = get_property_collection.update_one({"_id": property_id},{"$set": {"is_active": False}})
+        return deleted_property
+              
